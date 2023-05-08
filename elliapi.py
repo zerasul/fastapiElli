81ea211720dca6ef77f665f9ff6b3f7c79eba14d
zerasul <zerasul@gmail.com>
2023-05-08 08:08:11 +0200

    adding .gif to the end of url

--- elliapi.py
+++ elliapi.py
@@ -11,9 +11,9 @@ def get_random_gif(request=Request):
     domain= "localhost:8000"
     if 'MY_DOMAIN' in environ:
         domain=environ['MY_DOMAIN']
-    return {"id": id,"url":f'https://{domain}/api/gif/{id}'}
+    return {"id": id,"url":f'https://{domain}/api/gif/{id}.gif'}
 
-@app.get("/api/gif/{id}")
+@app.get("/api/gif/{id}.gif")
 def get_gif(id:int):
 
     return Response(content=readFile(id),media_type="image/gif")
